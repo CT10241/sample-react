e959605744238df2ab892b590aa891de6c252966
bhavya <bhavya.gupta@citiustech.com>
2024-01-03 14:03:23 +0530

    test

--- sample-react-app/src/App.js
+++ sample-react-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
 
       <h> Netflix clone</h>
-
+    <Row title="NETFLIX CLONE"/>
     </div>
   );
 }
